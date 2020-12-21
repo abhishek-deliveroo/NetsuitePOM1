b789f2571f83c3dd4a363ca9145136fdb88c2e2a
Abhishek <abhishek.dutta@tvarana.com>
2020-12-21 16:22:43 +0530

    9th Commit

--- DemoMavenProject/src/main/java/DemoMavenProject/DemoMavenProject/App.java
+++ DemoMavenProject/src/main/java/DemoMavenProject/DemoMavenProject/App.java
@@ -1,6 +1,6 @@
 package DemoMavenProject.DemoMavenProject;
 
-//Sample in the Application
+//Sample in the Application for Feature
 public class App 
 {
     public static void main( String[] args )
