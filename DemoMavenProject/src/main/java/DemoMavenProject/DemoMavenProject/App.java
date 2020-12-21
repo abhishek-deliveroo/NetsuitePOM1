f0c18df99444968d85337b2c9e9394bf4e58a7e7
Abhishek <abhishek.dutta@tvarana.com>
2020-12-21 16:59:21 +0530

    Commit 13th

--- DemoMavenProject/src/main/java/DemoMavenProject/DemoMavenProject/App.java
+++ DemoMavenProject/src/main/java/DemoMavenProject/DemoMavenProject/App.java
@@ -1,6 +1,6 @@
 package DemoMavenProject.DemoMavenProject;
 
-//Sample in the Application to Feature branch from Master
+//Sample in the Application to Feature branch from Master Branch 
 public class App 
 {
     public static void main( String[] args )
